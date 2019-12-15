b45fac7d317b51b50c88fa74aa9dd4cb4a5367e5
Ohidurr <Ohidurrahman96@gmail.com>
2019-12-15 10:59:41 -0500

    add api

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded",() => {
-    
+    const getDeck = async() => {
+        try{
+            let response = await axios.get("https://deckofcardsapi.com/api/deck/new/")
+            let deckID = response.data.deckID
+            let shuffle = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`);
+            let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`);
+        }catch(error){
+            console.log("You ran into a error! oh no!");
+            debugger
+        }
+    }
 })
