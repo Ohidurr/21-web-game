57a9e0420ffcfd976850aaf5a0f5ac9f7fef4bc4
Ohidurr <Ohidurrahman96@gmail.com>
2019-12-15 13:24:45 -0500

    key into objects

--- index.js
+++ index.js
@@ -5,19 +5,36 @@ document.addEventListener("DOMContentLoaded",() => {
     const getDeck = async() => {
         try{
             let response = await axios.get("https://deckofcardsapi.com/api/deck/new/")
-            let deck_id = response.data.deck_id
+            let deck_id = res.data.deck_id
+            let num = res.data.num
             let shuffle = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
-            let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`);
+            let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`);
+            debugger
         }catch(error){
             console.log(error + " this a error ");
+            //debugger
             
         }
         const displayCards = async () => {
+            try {
+            let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`);
+        }catch(error){
+            console.log("this is a error");
+            
+        }
+        for(let i = 0; i < num; i++){
           let img = document.createElement("img")
+          let url = draw.data.cards.image
+          img.src = url 
+          document.body.appendChild(img)
+        }
           
         }
+        // const getValue(deck_id) => {
+        //     let score = response.
+        // }
     }
-    displayCards()
+    //displayCards()
     getDeck()
 })
 
