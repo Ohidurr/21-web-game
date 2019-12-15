7ce969c8d921f3a068a66095b2726e30198fa8d8
Ohidurr <Ohidurrahman96@gmail.com>
2019-12-15 17:24:43 -0500

    add computer and some divs

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 // alert(button.outerHTML)
 document.addEventListener("DOMContentLoaded",() => {
     let deck_id
-    let human 
+    let humanScore = 0
+    let compScore = 0
     let computer
+    let human
     const getDeck = async() => {
         try{
             let res = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
@@ -19,18 +21,28 @@ document.addEventListener("DOMContentLoaded",() => {
         }
     }
         getDeck()
-        const setValue = (deck_id,score) => {
-            for (let i = 0; i < num; i++){
-            let value = draw.data.cards[i].value
-           if(draw.cards.value === "JACK" || draw.cards.value === "QUEEN" || draw.cards.value === "KING")
+        const setValue = (value,score) => {
+            if(value === "JACK" || value === "QUEEN" || value === "KING"){
             score += 10;
+            }else if(value === "ACE"){
+                 if(score <= 10){
+                     score += 11
+                 }else{
+                     score +=1
+                 }
+             }else{
+                 score += Number(value)
              }
+             return score;
             }
-        const draw = async (deck_id, num, player, score) => {
+        const draw = async (deck_id, num, score) => {
         let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`);
         for (let i = 0; i < num; i++){
             let value = draw.data.cards[i].value
-            let score = value;
+            score = setValue(value,score)
+            let h2 = document.createElement("h2")
+            h2.innerText = score;
+            document.body.appendChild(h2)
             let img = document.createElement("img")
             let url = draw.data.cards[i].image
             img.src = url 
@@ -38,42 +50,41 @@ document.addEventListener("DOMContentLoaded",() => {
             
           }
         }
-        const winCND = (player,score) =>{
-            let humanScore = score1
-            let compScore = score2
-            if(humanScore > compScore && humanScore < 21){
-                console.log("Congrat human, you won!");
+        // const winCND = (score) =>{
+            
+        //     if(human > comp && human < 21){
+        //         console.log("Congrat human, you won!");
                 
-            }else if(compScore > humanScore && compScore < 21){
-                console.log("Better luck next time");
+        //     }else if(comp > human && comp < 21){
+        //         console.log("Better luck next time");
                 
-            }else if(playerScore > 21){
-                console.log("BUSTED");
+        //     }else if(human> 21){
+        //         console.log("BUSTED");
                 
-            }else if(compScore > 21){
-                console.log("you Win, computer loses");
+        //     }else if(comp > 21){
+        //         console.log("you Win, computer loses");
                 
-            }
+        //     }
 
 
-        }
+        // }
+        
         let start = document.querySelector("#start")
         start.addEventListener("click",() =>{
-            draw(deck_id,2,human,score)
+            draw(deck_id,2,humanScore)
         })
         let hit = document.querySelector("#hit")
         hit.addEventListener("click",() => {
-            draw(deck_id,1,human,score) 
+            draw(deck_id,1,humanScore) 
         })
         let stay = document.querySelector("#stay")
         stay.addEventListener("click",() => {
-            draw(deck_id,3,computer,score)
+            draw(deck_id,3,compScore)
         })
         
     
     //displayCards(draw, 2)
 })
-
 // if compScore < userScore === 21 (win)
 //  compScore < userScore  < 21 (win)
 // compScore > 21 === win
