62b816c3223d57560d980d8ba5d272e9df6c738f
Ohidurr <Ohidurrahman96@gmail.com>
2019-12-16 21:47:54 -0500

    made both scores visible

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 document.addEventListener("DOMContentLoaded",() => {
     let deck_id
     let humanScore = 0
-    let compScore = 0
+    let computerScore = 0
     let computer;
     let human;
     const getDeck = async() => {
@@ -20,35 +20,50 @@ document.addEventListener("DOMContentLoaded",() => {
             
         }
     }
-        getDeck()
-        const setValue = (value,score) => {
+     getDeck()
+        const setValue = (value) => {
+            let score = 0;
             if(value === "JACK" || value === "QUEEN" || value === "KING"){
-            score += 10;
+            score = 10;
             }else if(value === "ACE"){
                  if(score <= 10){
-                     score += 11
+                     score = 11
                  }else{
-                     score +=1
+                     score =1
                  }
              }else{
-                 score += Number(value)
+                 score = Number(value)
              }
              return score;
             }
-        const draw = async (deck_id, num, score, player) => {
+        const draw = async (deck_id, num, who,div) => {
+        let parent = document.querySelector(`${div}`);
+        let final;
+        let score = 0;
         let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`);
         for (let i = 0; i < num; i++){
             let value = draw.data.cards[i].value
-            score = setValue(value,score)
-            let h2 = document.createElement("h2")
-            h2.innerText = score;
-            document.body.appendChild(h2)
+             score += setValue(value)
+            
             let img = document.createElement("img")
             let url = draw.data.cards[i].image
             img.src = url 
-            document.body.appendChild(img)
+            parent.appendChild(img)
             
           }
+          if(who ==='human'){
+            
+            humanScore += score
+            final = humanScore
+            }else{
+            computerScore += score
+            final = computerScore
+            }
+        //   debugger
+          let h2 = document.querySelector(`#${who}_score`)
+        //   debugger
+          h2.innerText = final;
+        //   parent.appendChild(h2)
         }
         // const winCND = (score) =>{
             
@@ -71,15 +86,18 @@ document.addEventListener("DOMContentLoaded",() => {
         
         let start = document.querySelector("#start")
         start.addEventListener("click",() =>{
-            draw(deck_id,2,humanScore,human)
+            draw(deck_id,2,'human','#human')
         })
         let hit = document.querySelector("#hit")
         hit.addEventListener("click",() => {
-            draw(deck_id,1,humanScore,human) 
+
+            draw(deck_id,1,'human','#human') 
+            // debugger
         })
         let stay = document.querySelector("#stay")
         stay.addEventListener("click",() => {
-            draw(deck_id,3,compScore,computer)
+            draw(deck_id,3,'computer',"#computer")
+            
         })
         
     
